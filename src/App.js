7ebf22153b2336c9cc6c974d4c6c4bc815c8d15b
Khalid <khalidfarajdev@gmail.com>
2025-05-11 17:39:08 +0300

    Custom fonts using (@font-face)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import ToDoList from './Components/ToDoList';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { typographyClasses } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Kufam'],
+  },
+});
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#191b1f',
-        direction: 'rtl',
-      }}
-    >
-      <ToDoList />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div
+        className="App"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          backgroundColor: '#191b1f',
+          direction: 'rtl',
+        }}
+      >
+        <ToDoList />
+      </div>
+    </ThemeProvider>
   );
 }
 
